6743d5e33a57d9ad5fbab70d86deb905e6fad148
Anna Addei <anna.addei@gmail.com>
2016-10-20 10:45:47 +0000

    fixes

--- js/index.js
+++ js/index.js
@@ -1,31 +1,19 @@
 (function(){
 	
-	document,addEventListener('deviceready', onDeviceReady.bind(this), false);
+	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 	var pictureSource;
 	var destinationType;
 
 	function onDeviceReady(){
-		pictureSource = navigator.camera.pictureSource;
-		destinationType = navigator.camera.destinationType;
+		pictureSource = navigator.camera.PictureSourceType;
+		destinationType = navigator.camera.DestinationType;
 
 	document.getElementById("capturePhoto").onclick = function(){
 		navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
 			quality:50,
 			destinationType : destinationType.DATA_URL });
-	}
-
-	document.getElementById("barcode").onclick = function(){
-		cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
-            alert("encode success: " + success);
-          }, function(fail) {
-            alert("encoding failed: " + fail);
-          }
-      }
-
-     // document.getElementById("geolocationdata").onclick = function(){
-     // 	navigator.geolocation.getCurrentPosition(onSuccess, onError);
-     // }
-     
+	
+}
 };
 
 	function onPhotoDataSuccess (imageData) {
@@ -34,7 +22,51 @@
 		smallImage.src = "data:image/jpeg;base64," + imageData;
 	}
 
-	var onSuccess = function(position) {
+	function onFail (message){
+		alert('Failed because:' + message);
+	}
+
+
+})();
+
+
+(function(){
+	
+	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+
+	function onDeviceReady(){
+
+	document.getElementById("barcode").onclick = function(){
+			cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          "preferFrontCamera" : true, // iOS and Android 
+          "showFlipCameraButton" : true, // iOS and Android 
+          "prompt" : "Place a barcode inside the scan area", // supported on Android only 
+          "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
+          "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device 
+      }
+   );
+		      
+	}
+     // document.getElementById("geolocationdata").onclick = function(){
+     // 	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+     // }
+     
+};
+})();
+
+	
+
+	/*var onSuccess = function(position) {
         alert('Latitude: '          + position.coords.latitude          + '\n' +
               'Longitude: '         + position.coords.longitude         + '\n' +
               );
@@ -48,4 +80,4 @@
 	
        
 
-})();
+})();*/
