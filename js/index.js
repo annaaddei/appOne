2631232dd243873e73a796d2c5ea58789dc77ad1
Anna Addei <anna.addei@gmail.com>
2016-11-18 17:31:55 +0000

    barcode

--- js/index.js
+++ js/index.js
@@ -1,20 +1,33 @@
 (function(){
-	
+
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
 	function onDeviceReady(){
 
 	document.getElementById("barcode").onclick = function(){
-		cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com",
-			function(success) {
-				alert("encode success: " + success);
-			}, function(fail) {
-				alert("encoding failed: " + fail);
-			}
-		);
+
+		cordova.plugins.barcodeScanner.scan(onBarcodeSuccess, onBarcodeFail,
+            {
+                "preferFrontCamera": true,
+                "showFlipCameraButton": true,
+                "prompt": "Place a barcode inside the scan area",
+                "formats": "QR_CODE,PDF_417",
+                "orientation": "landscape"
+            });
 	
 }
 };
+
+function onBarcodeSuccess(result) {
+    alert("We got a barcode\n" +
+            "Result: " + result.text + "\n" +
+            "Format: " + result.format + "\n" +
+            "Cancelled: " + result.cancelled);
+}
+
+function onBarcodeFail(error) {
+    alert("Scanning failed: " + error);
+}
 
 
 
